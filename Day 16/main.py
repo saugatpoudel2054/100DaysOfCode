7703a485045a0e7315b4ffc490bd8b8ef04fb140
Saugat Poudel <saugatpoudel2054@gmail.com>
2020-11-27 16:46:17 +0545

    Added coffe machine in OOP

--- Day 16/main.py
+++ Day 16/main.py
@@ -1,28 +1,23 @@
-from turtle import Turtle, Screen
-from prettytable import PrettyTable
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-timmy = Turtle()
-print(timmy)
+coffee_maker = CoffeeMaker()
+menu = Menu()
+money_machine = MoneyMachine()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-timmy.shape('turtle')
-timmy.color('red')
-timmy.forward(100)
-my_screen.exitonclick()
 
-x = PrettyTable()
-x.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
-x.add_rows(
-    [
-        ["Adelaide", 1295, 1158259, 600.5],
-        ["Brisbane", 5905, 1857594, 1146.4],
-        ["Darwin", 112, 120900, 1714.7],
-        ["Hobart", 1357, 205556, 619.5],
-        ["Sydney", 2058, 4336374, 1214.8],
-        ["Melbourne", 1566, 3806092, 646.9],
-        ["Perth", 5386, 1554769, 869.4],
-    ]
-)
-
-print(x)
+while True:
+    choice = input('What would you like? (espresso/latte/cappuccino): ')
+    if choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+        continue
+    elif choice == 'off':
+        print("Turning off machine...")
+        break
+    else:
+        item = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(item):
+            if money_machine.make_payment(item.cost):
+                coffee_maker.make_coffee(item)
